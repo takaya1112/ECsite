4be3d06c32572c7bc8c9b8963aa21d9d7927ce17
Takahashiyuuri <haru.isk@gmail.com>
2018-11-02 21:37:10 +0900

    11/02

--- ecsite/src/com/internousdev/ecsite/util/DBConnector.java
+++ ecsite/src/com/internousdev/ecsite/util/DBConnector.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-	private static String driverName = "com.mysql.jdbc.Drier";
+	private static String driverName = "com.mysql.jdbc.Driver";
 	private static String url = "jdbc:mysql://localhost/ecsite";
 
 	private static String user = "root";
--- templete/src/com/internousdev/template/util/DBConnector.java
+++ templete/src/com/internousdev/template/util/DBConnector.java
@@ -5,11 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnector {
-	private static String driverName="com.mysql.jdbc.Driver";
-	private static String url="jdbc:mysql://localhost/ecsite";
+	private static String driverName = "com.mysql.jdbc.Driver";
+	private static String url = "jdbc:mysql://localhost/ecsite";
 
-	private static String user="root";
-	private static String password="mysql";
+	private static String user = "root";
+	private static String password = "mysql";
 
 	public Connection getConnection(){
 		Connection con = null;
